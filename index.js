294c19753986f0acc16d5f35d55e2d0768f8011b
rbtucker <rbtucker16@gmail.com>
2019-08-12 05:34:04 -0700

    refactor to make configuration and code customizations easier

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 module.exports = require('./orm.js');
+module.exports.MetaData = require('./main/MetaData.js').MetaData;
+module.exports.Model = require('./main/Model.js');
+module.exports.Repository = require('./main/Repository.js');
+module.exports.Authorizor = require('./auth/Authorizor.js');
+module.exports.FieldConstraint = require('./main/FieldConstraint.js');
+module.exports.WhereComparison = require('./main/WhereComprison.js');
+module.exports.OrderByEntry = require('./main/OrderByEntry.js');
