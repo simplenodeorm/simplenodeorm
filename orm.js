f24a52a315a87d9d4564a9514b3dd03abaa209e1
rbtucker <rbtucker16@gmail.com>
2019-09-03 08:40:16 -0700

    add some error handling

--- orm.js
+++ orm.js
@@ -606,7 +606,7 @@ function startApiServer() {
                             result = await repo.existsSync(params, options);
                             break;
                         case util.NEW_MODEL.toLowerCase():
-                            result = util.toDataTransferObject(new (require(appConfiguration.ormModuleRootPath + '/model/' + req.params.module)(md)));
+                            result = util.toDataTransferObject(new (require(appConfiguration.ormModuleRootPath + '/model/' + req.params.module))(md));
                             break;
                         default:
                             res.status(400).send('invalid method \'' + req.params.method + '\' specified');
