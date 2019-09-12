85dcb58a0749177557ce8e790e020d3f9b065f16
rbtucker <rbtucker16@gmail.com>
2019-09-12 08:04:50 -0700

    fix some issues caused by model refactor

--- main/Repository.js
+++ main/Repository.js
@@ -528,6 +528,7 @@ module.exports = class Repository {
             if (model.__isNew() || !(await this.exists(model, options))) {
                 result = await this.executeSql(sql, params, options);
                 if (result.error) {
+                    logger.logInfo('-------------->' + JSON.stringify(result))
                     util.throwError("InsertException", result.error);
                 }
                 if (result.insertId) {
