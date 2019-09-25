f1ca0d1c171117d1f9441cf26cd661c184eaeb59
rbtucker <rbtucker16@gmail.com>
2019-09-25 16:10:30 -0700

    move delete to post

--- orm.js
+++ orm.js
@@ -704,7 +704,6 @@ function startApiServer() {
                             break;
                         case util.DELETE.toLowerCase():
                             startTransaction(repo, options);
-                            logger.logInfo('----------------->a=' + JSON.stringify(req.body));
                             result = await repo.delete(populateModelObjectsFromRequestInput(req.body.modelInstances), options);
                             endTransaction(repo, result, options);
                             break;
