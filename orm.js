5d2e630d9ca5073e7ceb50fab943d7bd362b325a
rbtucker <rbtucker16@gmail.com>
2019-10-18 17:17:07 -0700

    update documentation

--- orm.js
+++ orm.js
@@ -210,7 +210,12 @@ function startApiServer() {
         apiServer.use(bodyParser.json({limit: '5MB'}));
         apiServer.use(cors());
         apiServer.use(cookieParser());
-
+        appServer.use(function(req, res, next) {
+            res.header('Access-Control-Allow-Origin', '*');
+            res.header('Access-Control-Allow-Credentials', true);
+            res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+            next();
+        });
         const authorizer = new (require(appConfiguration.authorizer));
 
         let server;
@@ -234,13 +239,6 @@ function startApiServer() {
         apiServer.all('*', async function (req, res, next) {
             if (logger.isLogDebugEnabled()) {
                 logger.logDebug("in /" + appConfiguration.context + ' checkAuthorization');
-            }
-
-            logger.logInfo('----->headers=' + JSON.stringify(req.headers));
-
-            if (req.query && req.query.key && myCache.get(req.query.key)) {
-                myCache.del(req.query.key);
-                next();
             }
             /*
             if (req.url.endsWith("/login")) {
