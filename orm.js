da95d3de4dc2d944dffac48f666b2c22f8325b98
rbtucker <rbtucker16@gmail.com>
2019-11-25 20:47:03 -0700

    update cors

--- orm.js
+++ orm.js
@@ -241,7 +241,9 @@ function startApiServer() {
 
         apiServer.use(function(req, res, next) {
             res.header("Access-Control-Allow-Origin", "*");
-            res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+            res.header("Access-Control-Allow-Credentials", "true");
+            res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT,DELETE");
+            res.header("Access-Control-Allow-Headers", "*");
             next();
         });
 
